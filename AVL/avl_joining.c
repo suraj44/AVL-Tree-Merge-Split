9e502476a08bcbb31ef61538a90e91779dce9a86
Suraj Singh <suraj1998@gmail.com>
2019-01-26 20:47:11 +0530

    h2>h1 error

--- AVL/avl_joining.c
+++ AVL/avl_joining.c
@@ -1,52 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "avl_utils.c"
+#include "avl_utils.h"
 
 int main(int argc, char const *argv[])
 {
-    int left[16] = {3,5,2,7,4,21,23,25,8,14,15,16,17,18,19,20};
-    int right[5]= {56,34,45,78,91};
+    int right[16] = {3,5,2,7,4,21,23,25,8,14,15,16,17,18,19,20};
+    int left[5]= {56,34,45,78,91};
 
     node *T1 = NULL, *T2= NULL;
     
-    for(size_t i = 0; i < 16; i++)
+    for(size_t i = 0; i < 5; i++)
     {
         T1 = insert(T1,left[i]);
     }
     
-    for(size_t i = 0; i < 5; i++)
+    for(size_t i = 0; i < 16; i++)
     {
         T2 = insert(T2, right[i]);
     }
     
     int h1 = T1->height;
     int h2 = T2->height;
-    
-    node * temp = T2;
-    while(temp->left) temp=temp->left;
-    int x = temp->val;
 
-    T2 = delete_node(T2, x);
-    int h = T2->height;
-    
-    node *v = T1, *u=T1;
-    int h_prime = h1;
-    while (h_prime >= h+1){
-        if(balance_factor(v)==-1) h_prime -= 2;
-        else h_prime -=1;
-        u = v;
-        v = v->right;
+    if(h1 < h2) {
+        node * temp = T2;
+        while(temp->left) temp=temp->left;
+        int x = temp->val;
+
+        T2 = delete_node(T2, x);
+        int h = T2->height;
+        
+        node *v = T1, *u=T1;
+        int h_prime = h1;
+        while (h_prime >= h+1){
+            if(balance_factor(v)==-1) h_prime -= 2;
+            else h_prime -=1;
+            u = v;
+            v = v->right;
+        }
+        
+        node* new_tree = new_node(x);
+        new_tree->left = v;
+        new_tree->right= T2;
+        new_tree->height = 1 + max(height(new_tree->left),height(new_tree->right));
+        
+        u->right = new_tree;
+        
+        printf("The resulting tree printed in inorder fashion :\n");
+        display(T1);
+    } else {
+        node * temp = T1;
+        while(temp->right) temp=temp->right;
+        int x = temp->val;
+
+        T1 = delete_node(T1, x);
+        int h = T1->height;
+
+        node *v = T2, *u=T2;
+        int h_prime = h2;
+        while (h_prime >= h+1){
+            if(balance_factor(v)==1) h_prime -= 2;
+            else h_prime -=1;
+            u = v;
+            v = v->left;
+        }
+
+        node* new_tree = new_node(x);
+        new_tree->right = v;
+        new_tree->left= T1;
+        new_tree->height = 1 + max(height(new_tree->left),height(new_tree->right));
+        
+        u->left = new_tree;
+        
+        printf("The resulting tree printed in inorder fashion :\n");
+        display(T2);
+
     }
     
-    node* new_tree = new_node(x);
-    new_tree->left = v;
-    new_tree->right= T2;
-    new_tree->height = 1 + max(height(new_tree->left),height(new_tree->right));
     
-    u->right = new_tree;
-    
-    printf("The resulting tree printed in inorder fashion :\n");
-    display(T1);
     
     return 0;
 }
