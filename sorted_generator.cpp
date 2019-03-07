87e48efdcf94c78210b56f87aa620dd6bec1d26f
Darshan D V <darshandv10@gmail.com>
2019-03-07 12:35:59 +0530

    Generator function modified

--- sorted_generator.cpp
+++ sorted_generator.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+	if(argc!=2){
+		cout<<"Expected format: ./<exe_file> <number of entries>"<<endl;
+		cout<<"Please reenter properly\n";
+		exit(0);
+	}
 	vector <unsigned int> v;
 	srand(time(NULL));
 	long long int i,num;
@@ -16,6 +21,14 @@ int main(int argc, char *argv[])
 	sort(v.begin(),v.end());
 	ofstream fout;
 	fout.open("AVL/sorted_dataset.txt");
+	for(i=0;i<num-1;i++)
+	{
+		fout<<v[i]<<endl;
+	}
+	fout<<v[i];
+	fout.close();
+
+	fout.open("Red-Black-Tree/sorted_dataset.txt");
 	for(i=0;i<num-1;i++)
 	{
 		fout<<v[i]<<endl;
